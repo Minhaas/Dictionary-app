9ee6c516c4689f07b16b07e512c483995fb64c7c
Minhaas <minhaas@Mohammeds-MacBook-Pro.local>
2020-09-07 19:12:57 +0530

    Backend completed

--- backend.py
+++ backend.py
@@ -1,35 +1,67 @@
 import sqlite3
 
 def connect():
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
     conn.commit()
     conn.close()
 
+def addEntry(title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
+    conn.commit()
+    conn.close()
+    view()
+
 def viewAll():
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
     cur.execute("SELECT * FROM book")
-    rows = cur.fetchall()
+    rows=cur.fetchall()
     conn.close()
     return rows
 
-def search(title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM book WHERE title=%s OR author=%s OR year=%s OR isbn-%s",(title, author, year, isbn))
-    rows = cur.fetchall() #This fetches all the values from the database, must be used for printing values. Dont have to commit the database in this case as nothing is being written by it  
+def search(title="",author="",year="",isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title,author,year,isbn))
+    rows=cur.fetchall()
     conn.close()
     return rows
 
-def addEntry(title, author, year, isbn):
-    conn = sqlite3.connect("books.db")
-    cur = conn.cursor()
-    cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)" , (title, author, year, isbn)) #ID in the database when assigned NULL get automatically updated by python
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+def update(id,title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
     conn.commit()
     conn.close()
 
 connect()
-addEntry("Half Girlfriend", "Chetan Bhagat", 2012, 1001)
+
+"""
+def deleteAll():
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE from book")
+    conn.commit()
+    conn.close()
+
+connect()
 print(viewAll())
+delete(3)
+update(6, "The Passion of Santos", "Rogelio de la vega", 2016, 1903939292)
+print(viewAll())
+#deleteAll()
+print(viewAll())
+"""
+
+
